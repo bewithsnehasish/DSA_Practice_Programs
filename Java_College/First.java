37449c7a445193b2ca7f9922c051ccee5117c9e7
Snehasish Mandal <mandalsneh97@gmail.com>
2025-01-14 09:57:32 +0530

    Initialised the first java program

---
+++ Java_College/First.java
@@ -0,0 +1,16 @@
+class First {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+} 
+
